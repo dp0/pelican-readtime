5bc140b7400e16f69aef3379c359ed07f84fee11
Daniel Playle <code@danplayle.com>
2023-11-08 20:56:30 +0000

    Restructure and add functionality to ignore tags
    This commit repo per the Pelican plugin documentation.
    
    This commit also introduces new functionality to ignore some tag
    contents. These ignored tags are configured with the `pelicanconf.py`
    variable `READTIME_IGNORE_TAGS` variable, such as:
    
    ```pelicanconf.py
    READTIME_IGNORE_TAGS = ['svg']
    ```

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pelican_readtime',
-    version='0.3.0',
+    version='0.3.3',
     packages=find_packages(),
     install_requires=[]
 )
